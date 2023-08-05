5f10399e95299e6d98867c8e0a9ea8b2820adf9f
Yulia <Yulia.mochalova.2001@mail.ru>
2023-08-05 21:38:57 +0300

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+import {getFirestore} from 'firebase/firestore'
+import {getStorage} from 'firebase/storage'
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCdglbEfpxovRaNrqB-atqPqb4-B5DiF8c",
+  authDomain: "eshop-bf681.firebaseapp.com",
+  projectId: "eshop-bf681",
+  storageBucket: "eshop-bf681.appspot.com",
+  messagingSenderId: "355198821085",
+  appId: "1:355198821085:web:a08518ac9753fe708a7134"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app) 
+export const bd = getFirestore(app) 
+export const storage = getStorage(app) 
+
+export default app
