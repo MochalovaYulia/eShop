4ff30d270b28750bbf3547b6f66b0849c210f8a0
Yulia <Yulia.mochalova.2001@mail.ru>
2023-08-05 21:56:06 +0300

    loader component

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Loader.module.scss'
+import loader from '../../assets/loader.gif'
+import { ReactDOM } from 'react-dom'
+
+export const Loader = () => {
+  return ReactDOM.createPortal (
+    <div className={styles.wrapper}>
+        <div className={styles.loader}>
+            <img src={loader} alt='Loading...'/>
+        </div>
+    </div>,
+    document.getElementById('loader')
+  )
+}
