8bb30ff72eee092a01f37de8365ebafb9669d871
Yulia <Yulia.mochalova.2001@mail.ru>
2023-09-03 19:26:51 +0300

    Fetch products in products Component

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Product.module.scss'
 import { ProductFilter } from './productFilter/productFilter'
 import { ProductList } from './productList/productList'
+import { useDispatch, useSelector } from 'react-redux'
+import { useFetchCollection } from '../../customHooks/useFetchCollection'
+import { selectProduct, store_products } from '../../redux/slice/productSlice'
 
 export const Product = () => {
+
+    const { data } = useFetchCollection('product')
+    const products = useSelector(selectProduct)
+    
+    const dispatch = useDispatch()
+  
+    useEffect(() => {
+      dispatch(
+        store_products({
+          products: data,
+        })
+      );
+    }, [dispatch, data])
+
+
     return (
         <section>
             <div className={`container ${styles.product}`}>
@@ -12,7 +30,7 @@ export const Product = () => {
                 </aside>
 
                 <div className={styles.content}>
-                    <ProductList />
+                    <ProductList products={products}/>
                 </div>
             </div>
         </section>
