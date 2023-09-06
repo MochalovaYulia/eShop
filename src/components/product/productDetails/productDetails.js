c6a6f09fdd55fa83fd1fad84cbab94d2a97cac41
Yulia <Yulia.mochalova.2001@mail.ru>
2023-09-06 22:10:35 +0300

    Display Product on Product Details Page~

--- src/components/product/productDetails/productDetails.js
+++ src/components/product/productDetails/productDetails.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import styles from './productDetails.module.scss'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { doc, getDoc } from 'firebase/firestore'
 import { db } from '../../../firebase/config'
+import spinnerImg from '../../../assets/spinner.jpg'
 
 export const ProductDetails = () => {
     const { id } = useParams()
@@ -17,13 +18,52 @@ export const ProductDetails = () => {
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-            console.log("Document data:", docSnap.data());
+            const obj = {
+                id: id,
+                ...docSnap.data(),
+            }
+            setProduct(obj)
         } else {
             console.log("No such document!");
         }
     }
 
     return (
-        <div>productDetails</div>
+        <section>
+            <div className={`container ${styles.product}`}>
+                <h2>productDetails</h2>
+                <div>
+                    <Link to={'#/products'}>&larr; Back to products</Link>
+                </div>
+                {product === null ? (
+                    <img src={spinnerImg} alt='Loading...' />
+                ) : (
+                    <>
+                        <div className={styles.details}>
+                            <div className={styles.img}>
+                                <img src={product.imageURL} alt={product.name} />
+                            </div>
+                            <div className={styles.content}>
+                                <h3>{product.name}</h3>
+                                <p className={styles.price}>{`$${product.price}`}</p>
+                                <p>{product.desc}</p>
+                                <p>
+                                    <b>SKU</b> {product.id}
+                                </p>
+                                <p>
+                                    <b>Brand</b> {product.brand}
+                                </p>
+                                <div className={styles.count}>
+                                    <button className='--btn'>-</button>
+                                    <p>1</p>
+                                    <button className='--btn'>+</button>
+                                </div>
+                                <button className='--btn --btn-danger'>ADD TO CART</button>
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>
+        </section>
     )
 }
