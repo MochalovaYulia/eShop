c7c8cc1bbaf776e90dda7869a8b2704f89dac56f
Yulia <Yulia.mochalova.2001@mail.ru>
2023-09-06 23:03:16 +0300

    Product Filter JSX

--- src/components/product/productFilter/productFilter.js
+++ src/components/product/productFilter/productFilter.js
@@ -3,6 +3,24 @@ import styles from './productFilter.module.scss'
 
 export const ProductFilter = () => {
   return (
-    <div>productFilter</div>
+    <div className={styles.filter}>
+      <h4>Categories</h4>
+      <div className={styles.category}>
+        <button>All</button>
+      </div>
+      <h4>Brand</h4>
+      <div className={styles.brand}>
+        <select name='brand'>
+          <option value='all'>All</option>
+        </select>
+      </div>
+      <h4>Price</h4>
+      <div className={styles.price}>
+        <p>1500</p>
+        <input type='range' name='price' min={100} max={10000} />
+      </div>
+      <br />
+      <button className='--btn --btn-danger'>Clear Filters</button>
+    </div>
   )
 }
