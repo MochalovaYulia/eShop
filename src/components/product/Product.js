cec3996d8b1a42088f53bca40ea8d19f99165893
Yulia <Yulia.mochalova.2001@mail.ru>
2023-09-18 21:53:12 +0300

    Make the Product Filter Responsive

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Product.module.scss'
+import {FaCogs} from 'react-icons/fa'
 import { ProductFilter } from './productFilter/productFilter'
 import { ProductList } from './productList/productList'
 import { useDispatch, useSelector } from 'react-redux'
@@ -8,7 +9,7 @@ import { get_price_range, selectProduct, store_products } from '../../redux/slic
 import spinnerImg from '../../assets/spinner.jpg'
 
 export const Product = () => {
-
+  const [showFilter, setShowFilter] = useState(false)
   const { data, isLoading } = useFetchCollection('product')
   const products = useSelector(selectProduct)
 
@@ -28,10 +29,14 @@ export const Product = () => {
     )
   }, [dispatch, data])
 
+  const toggleFilter = () => {
+    setShowFilter(!showFilter)
+  }
+
   return (
     <section>
       <div className={`container ${styles.product}`}>
-        <aside className={styles.filter}>
+        <aside className={showFilter ? `${styles.filter} ${styles.show}`: `${styles.filter}`}>
           {isLoading ? null : <ProductFilter />}
         </aside>
 
@@ -41,6 +46,12 @@ export const Product = () => {
           ) : (
             <ProductList products={products} />
           )}
+          <div className={styles.icon} onClick={toggleFilter}>
+            <FaCogs size={20} color='orangered' />
+            <p>
+              <b>{showFilter ? 'Hide Filter' : 'Show Filter'}</b>
+            </p>
+          </div>
         </div>
       </div>
     </section>
